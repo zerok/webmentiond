8aa0bf1b137685682aad503dce2f92b9f1960ef9
Horst Gutmann <zerok@zerokspot.com>
2020-07-13 21:11:55 +0200

    Add documentation to policies package

--- pkg/policies/policies.go
+++ pkg/policies/policies.go
@@ -9,6 +9,7 @@ import (
 	"github.com/rs/zerolog"
 )
 
+// Loader is used to load policies into a registry from various sources.
 type Loader interface {
 	Load(context.Context) ([]URLPolicy, error)
 }
@@ -21,16 +22,21 @@ func (s *staticLoader) Load(ctx context.Context) ([]URLPolicy, error) {
 	return s.policies, nil
 }
 
+// StaticLoader implements the Loader interface and fills a registry with the
+// given policies.
 func StaticLoader(p []URLPolicy) Loader {
 	return &staticLoader{policies: p}
 }
 
+// Policy is an action name that should be applied to an URL.
 type Policy string
 
 const REJECT = Policy("reject")
 const APPROVE = Policy("approve")
 const DEFAULT = Policy("default")
 
+// URLPolicy is a combination of a Policy that should be applied to a given URL
+// pattern.
 type URLPolicy struct {
 	ID         int
 	URLPattern *regexp.Regexp
@@ -38,12 +44,14 @@ type URLPolicy struct {
 	Weight     int
 }
 
+// Registry acts as container for policies.
 type Registry struct {
 	defaultPolicy Policy
 	policies      []URLPolicy
 	lock          sync.RWMutex
 }
 
+// NewRegistry creates a new registry and sets a default policy.
 func NewRegistry(defaultPolicy Policy) *Registry {
 	return &Registry{
 		defaultPolicy: defaultPolicy,
@@ -51,6 +59,7 @@ func NewRegistry(defaultPolicy Policy) *Registry {
 	}
 }
 
+// Policies returns a copy of all the policies inside the registry.
 func (r *Registry) Policies() []URLPolicy {
 	res := append(make([]URLPolicy, 0, len(r.policies)), r.policies...)
 	return res
@@ -72,6 +81,8 @@ func (b byWeight) Less(i, j int) bool {
 	return b[i].Weight < b[j].Weight
 }
 
+// AddPolicy creates a new policy given the parameters and adds it to the
+// registry.
 func (r *Registry) AddPolicy(u string, policy Policy, weight int) error {
 	r.lock.Lock()
 	defer r.lock.Unlock()
@@ -88,6 +99,7 @@ func (r *Registry) AddPolicy(u string, policy Policy, weight int) error {
 	return nil
 }
 
+// Load uses the given loader to fill the registry with policies.
 func (r *Registry) Load(ctx context.Context, loader Loader) error {
 	logger := zerolog.Ctx(ctx)
 	result, err := loader.Load(ctx)
@@ -106,6 +118,7 @@ func (r *Registry) Load(ctx context.Context, loader Loader) error {
 	return nil
 }
 
+// DetermineForURL returns a policy that should apply to the given URL.
 func (r *Registry) DetermineForURL(u string) Policy {
 	r.lock.RLock()
 	defer r.lock.RUnlock()
