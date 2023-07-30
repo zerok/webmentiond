ccd4bfb8068d5d323cd719ccb17c70e0ddf0ff0f
Horst Gutmann <zerok@zerokspot.com>
2023-07-30 22:04:39 +0200

    WIP: CI: More debug output during rsync

--- ci/website.go
+++ ci/website.go
@@ -18,9 +18,10 @@ func runBuildWebsite(ctx context.Context, dc *dagger.Client, opts buildWebsiteOp
 		_, err := dc.Container().From(alpineImage).
 			WithExec([]string{"apk", "add", "--no-cache", "rsync", "openssh"}).
 			WithMountedSecret("/root/.ssh/id_rsa", opts.sshPrivateKey).
+			WithExec([]string{"chmod", "0600", "/root/.ssh/id_rsa"}).
 			WithDirectory("/src", container.Directory("/data/site")).
 			WithWorkdir("/src").
-			WithExec([]string{"rsync", "-e", "ssh -o StrictHostKeyChecking=no", "-avz", ".", "www-webmentiondorg@webmentiond.org:/srv/www/webmentiond.org/www/htdocs/"}).
+			WithExec([]string{"rsync", "-e", "ssh -vvv -o StrictHostKeyChecking=no", "-avz", ".", "www-webmentiondorg@webmentiond.org:/srv/www/webmentiond.org/www/htdocs/"}).
 			ExitCode(ctx)
 		return err
 	} else {
